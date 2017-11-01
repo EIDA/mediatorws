114e4c3ddff02e5094ff9838e9a881e189b31d71
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2017-11-01 09:19:19 +0100

    proceeding WFCatalog implementation

--- federator/utils/misc.py
+++ federator/utils/misc.py
@@ -15,6 +15,11 @@ import tempfile
 
 import marshmallow as ma
 
+from webargs import core
+from webargs.flaskparser import FlaskParser
+
+from federator import settings
+
 dateutil_available = False
 try:
     from dateutil import parser
@@ -52,6 +57,71 @@ class CustomParser(argparse.ArgumentParser):
         sys.exit(ExitCodes.EXIT_ERROR)
 
 # class CustomParser
+
+
+class FDSNWSParser(FlaskParser):
+
+    STREAM_BUFFER = ''
+
+    def parse_form(self, req, name, field):
+
+        buf = req.stream.read()
+        if buf:
+            FDSNWSParser.STREAM_BUFFER = buf
+
+        print(FDSNWSParser.STREAM_BUFFER)
+        # convert buffer into list
+        req_buffer = FDSNWSParser.STREAM_BUFFER.split("\n")
+        print(req_buffer)
+
+        param_dict = {}
+        sncls = []
+
+        for line in req_buffer:
+            check_param = line.split(settings.FDSNWS_QUERY_VALUE_SEPARATOR_CHAR)
+            if len(check_param) == 2:
+                
+                # add query params
+                param_dict[check_param[0].strip()] = check_param[1].strip()
+                #self.logger.debug('Query parameter: %s' % check_param)
+            elif len(check_param) == 1:
+                sncl = line.split()
+                if len(sncl) == 6:
+                    sncls.append(line.strip())
+            else:
+                #self.logger.warn("Ignoring illegal POST line: %s" % line)
+                continue
+
+            """
+            elif len(check_param) == 1:
+                # parse SNCL
+                sncl = line.split()
+                if len(sncl) == 6:
+                    sncl_dict = {}
+                    sncl_dict['network'] = sncl[0]
+                    sncl_dict['station'] = sncl[1]
+                    sncl_dict['location'] = sncl[2]
+                    sncl_dict['channel'] = sncl[3]
+                    sncl_dict['starttime'] = sncl[4]
+                    sncl_dict['endtime'] = sncl[5]
+
+                    return core.get_value(sncl_dict, name, field)
+
+                else:
+                    # TODO(damb): raise an error
+                    pass 
+                    #self.logger.debug('Adding POST content: "%s"' % line)
+            """
+        if sncls:
+            param_dict['sncls'] = sncls
+        return core.get_value(param_dict, name, field)
+
+    # parse_form ()
+
+# class FDSNWSParser
+
+fdsnws_parser = FDSNWSParser()
+use_fdsnws_args = fdsnws_parser.use_args
 
 # -----------------------------------------------------------------------------
 def get_temp_filepath():
