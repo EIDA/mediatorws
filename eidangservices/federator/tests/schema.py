65659482b762da78438739e839bca601b1d4c05b
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2017-12-06 15:22:15 +0100

    license notices

--- eidangservices/federator/tests/schema.py
+++ eidangservices/federator/tests/schema.py
@@ -3,6 +3,24 @@
 # This is <schema.py>
 # -----------------------------------------------------------------------------
 #
+# This file is part of EIDA NG webservices (eida-federator).
+# 
+# eida-federator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version.
+#
+# eida-federator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ----
+# 
+# Copyright (c) Daniel Armbruster (ETH), Fabian Euchner (ETH)
+#
 # REVISION AND CHANGES
 # 2017/11/16        V0.1    Daniel Armbruster
 #
