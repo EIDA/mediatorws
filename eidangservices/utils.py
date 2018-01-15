cb5b36f102936c51fc3ae2bb7abc2559b9352650
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2018-01-15 10:26:56 +0100

    adjusted gpl comments

--- eidangservices/utils.py
+++ eidangservices/utils.py
@@ -3,14 +3,14 @@
 # This is <utils.py>
 # -----------------------------------------------------------------------------
 #
-# This file is part of EIDA NG webservices (eida-federator).
+# This file is part of EIDA NG webservices.
 # 
-# eida-federator is free software: you can redistribute it and/or modify
+# EIDA NG webservices is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or 
 # (at your option) any later version.
 #
-# eida-federator is distributed in the hope that it will be useful,
+# EDIA NG webservices is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
