c82e77abab7f14763ec1fc63a86f117727dd3451
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2018-01-16 12:04:33 +0100

    corrected comments

--- eidangservices/tests/utils.py
+++ eidangservices/tests/utils.py
@@ -3,22 +3,22 @@
 # This is <utils.py>
 # -----------------------------------------------------------------------------
 #
-# This file is part of EIDA NG webservices (eida-federator).
-# 
-# eida-federator is free software: you can redistribute it and/or modify
+# This file is part of EIDA NG webservices.
+#
+# EIDA NG webservices is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or 
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# eida-federator is distributed in the hope that it will be useful,
+# EIDA NG webservices is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # ----
-# 
+#
 # Copyright (c) Daniel Armbruster (ETH), Fabian Euchner (ETH)
 #
 # REVISION AND CHANGES
@@ -26,7 +26,7 @@
 #
 # =============================================================================
 """
-Federator utility test facilities.
+EIDA NG webservices utility test facilities.
 """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
