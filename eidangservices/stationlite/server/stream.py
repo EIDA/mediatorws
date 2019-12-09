f48cf7117be056d6a8ed03b16067901eac853bad
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2019-12-09 16:52:17 +0100

    [ENH] Use list for instead of str +=

--- eidangservices/stationlite/server/stream.py
+++ eidangservices/stationlite/server/stream.py
@@ -53,21 +53,17 @@ class PostStream(OutputStream):
         return ' '.join(PostStream.SERIALIZER.dump(stream_epoch).values())
 
     def __str__(self):
-        retval = ''
+        lines = []
         for url, stream_epoch_lst in self.routes:
             # add url netloc prefix
             if self._netloc_proxy:
                 url = self.prefix_url(url)
 
-            if retval:
-                retval += '\n\n'
-            retval += url + '\n' + '\n'.join(self._serialize(se)
-                                             for se in stream_epoch_lst)
+            lines.append(url)
+            lines.extend('%s' % self._serialize(se) for se in stream_epoch_lst)
+            lines.append('')
 
-        if retval:
-            retval += '\n'
-
-        return retval
+        return '\n'.join(lines)
 
 
 class GetStream(OutputStream):
