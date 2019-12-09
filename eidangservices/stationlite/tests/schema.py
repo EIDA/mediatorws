b5ddc689d45bbc7da5cc23e689d8574c2c72f5c4
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2019-12-09 13:27:48 +0100

    [TST] Add unittests

--- eidangservices/stationlite/tests/schema.py
+++ eidangservices/stationlite/tests/schema.py
@@ -5,14 +5,19 @@ Field and schema related test facilities.
 
 import unittest
 
+from marshmallow import ValidationError
+
 from eidangservices.stationlite.server import schema
 
 
 # -----------------------------------------------------------------------------
 class StationLiteSchemaTestCase(unittest.TestCase):
 
+    def create_schema(self, *args, **kwargs):
+        return schema.StationLiteSchema(*args, **kwargs)
+
     def test_geographic_opts(self):
-        s = schema.StationLiteSchema()
+        s = self.create_schema()
         reference_result = {
             'alternative': 'false',
             'service': 'dataselect',
@@ -37,6 +42,68 @@ class StationLiteSchemaTestCase(unittest.TestCase):
         self.assertEqual(result, reference_result)
         result = s.dump(s.load(test_datasets[1]))
         self.assertEqual(result, reference_result)
+
+    def test_proxy_netloc(self):
+        s = self.create_schema()
+
+        reference_result = {
+            'alternative': 'false',
+            'service': 'dataselect',
+            'format': 'post',
+            'level': 'channel',
+            'nodata': 204,
+            'proxynetloc': 'www.example.com:8888',
+            'minlatitude': -90.0,
+            'maxlatitude': 90.0,
+            'minlongitude': -180.0,
+            'maxlongitude': 180.0,
+        }
+
+        self.assertEqual(
+            s.load({'proxynetloc': '//www.example.com:8888'}),
+            reference_result)
+
+    def test_proxy_netloc_err(self):
+        s = self.create_schema()
+
+        with self.assertRaises(ValidationError):
+            s.load({'proxynetloc': 'www.example.com:8888'})
+
+    def test_proxy_netloc_missing(self):
+        s = self.create_schema()
+
+        reference_result = {
+            'alternative': 'false',
+            'service': 'dataselect',
+            'format': 'post',
+            'level': 'channel',
+            'nodata': 204,
+            'proxynetloc': None,
+            'minlatitude': -90.0,
+            'maxlatitude': 90.0,
+            'minlongitude': -180.0,
+            'maxlongitude': 180.0,
+        }
+
+        self.assertEqual(s.load({}), reference_result)
+
+    def test_proxy_netloc_none(self):
+        s = self.create_schema()
+
+        reference_result = {
+            'alternative': 'false',
+            'service': 'dataselect',
+            'format': 'post',
+            'level': 'channel',
+            'nodata': 204,
+            'proxynetloc': None,
+            'minlatitude': -90.0,
+            'maxlatitude': 90.0,
+            'minlongitude': -180.0,
+            'maxlongitude': 180.0,
+        }
+
+        self.assertEqual(s.load({'proxynetloc': None}), reference_result)
 
 
 # -----------------------------------------------------------------------------
