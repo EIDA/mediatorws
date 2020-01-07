a75d93faa6e3a64add51e11322b5916ebf6db0a9
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2020-01-07 15:34:34 +0100

    [STY] Remove GPL phrase and style comments

--- setup.py
+++ setup.py
@@ -1,29 +1,4 @@
 # -*- coding: utf-8 -*-
-# -----------------------------------------------------------------------------
-# This is <setup.py>
-# -----------------------------------------------------------------------------
-#
-# This file is part of EIDA NG webservices.
-#
-# EIDA NG webservices are free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# EIDA NG webservices are distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# ----
-#
-# Copyright (c) Daniel Armbruster (ETH), Fabian Euchner (ETH)
-#
-# REVISION AND CHANGES
-# 2017/11/26        V0.1    Daniel Armbruster
-# =============================================================================
 """
 setup.py for EIDA NG Mediator/Federator webservices
 """
@@ -235,5 +210,3 @@ setup(
             'version': ('setup.py', _version),
             'release': ('setup.py', _version)}},
 )
-
-# ---- END OF <setup.py> ----
